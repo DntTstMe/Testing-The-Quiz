843fdea55dff6a2227cf1655bf8b9d2151dac805
Alex Tester <lex.tester93@gmail.com>
2023-06-21 14:05:37 -0400

    Add questions for quiz to the questions.js file. Each answer has a wrong/right indicator with the corresponding question being asked.

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+// seperate file containing the questions and answers
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:', //question title
+        options: [
+            { text: 'strings', correct: false},// Option 1 with text and wrong/right indicator
+            { text: 'booleans', correct: false},// Option 2 with text and wrong/right indicator
+            { text: 'alerts', correct: true},// Option 3 with text and wrong/right indicator
+            { text: 'numbers', correct: false}// Option 4 with text and wrong/right indicator
+        ]
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within ___.',
+        options: [
+            { text: 'quotes', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'parentheses', correct: true},
+            { text: 'square brackets', correct: false}
+        ]
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ___.',
+        options: [
+            { text: 'numbers and strings', correct: false},
+            { text: 'other arrays', correct: false},
+            { text: 'booleans', correct: false},
+            { text: 'all of the above', correct: true}
+        ],
+    },
+    {
+        title: 'String values must be enclosed within ___ when being assigned to variables.',
+        options: [
+            { text: 'commas', correct: false},
+            { text: 'curly brackets', correct: false},
+            { text: 'quotes', correct: true},
+            { text: 'parentheses', correct: false}
+        ]
+    },
+    {
+        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        options: [
+            { text: 'JavaScript', correct: false},
+            { text: 'terminal / bash', correct: false},
+            { text: 'for loops', correct: false},
+            { text: 'console.log', correct: true}
+        ]
+    }
+];
