e9ae2505ac6db380cf98a95586b68b8c9d722951
Alex Tester <lex.tester93@gmail.com>
2023-06-21 14:22:24 -0400

    Initialized variables in DOM using getElementById

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var startScreenEl = document.getElementById("start-screen");
+var endScreenEl = document.getElementById("end-screen");
+var questionTitleEl = document.getElementById("question-title");
+var answerButtonsEl = document.getElementById("options");
+var feedbackEl = document.getElementById("feedback");
+var counter = document.getElementById("timer");
