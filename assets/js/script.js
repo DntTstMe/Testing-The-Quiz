620102da3ab8cd81d239ef560bf84d91f7dcb72a
Alex Tester <lex.tester93@gmail.com>
2023-06-21 17:18:14 -0400

    Added function that starts the quiz and randomizes the questions. It also calls the timer function to set and start the timer. Calls getQuestion to display first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // variables to reference DOM elements
+var questionTitleEl = document.getElementById("question-title");
 var questionsEl = document.getElementById("questions");
 var startScreenEl = document.getElementById("start-screen");
 var endScreenEl = document.getElementById("end-screen");
-var questionTitleEl = document.getElementById("question-title");
 var answerButtonsEl = document.getElementById("options");
 var feedbackEl = document.getElementById("feedback");
 var counter = document.getElementById("timer");
@@ -10,3 +10,17 @@ var counter = document.getElementById("timer");
 // variables to keep track of quiz state
 let shuffledQuestions, currentQuestionIndex;
 var seconds = 60
+
+// starts quiz
+function startQuiz() {
+    // hides start screen and unhides questions screen
+    startScreenEl.classList.add('hide');
+    questionsEl.classList.remove('hide');
+
+    // randomizes questions and creates a randomized array
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+
+    setTimer();
+    getQuestion();
+}
